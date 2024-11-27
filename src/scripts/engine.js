39bd57b9f9fd5dc5af293d5a1be21c81ac970531
dlucioyauh <dlucio.douglas@gmail.com>
2024-11-27 08:49:59 -0300

    Implementando o removeAllCardsImages

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,6 +13,12 @@ const state ={
         player: document.getElementById("player-field-card"),
         computer: document.getElementById("computer-field-card"),
     },
+    playerSides: {
+        player1: "player-cards",
+        player1Box: document.querySelector("#player-cards"),
+        computer: "computer-cards",
+        computerBox: document.querySelector("#computer-cards"),
+    },
     actions:{
         button: document.getElementById("next-duel"),
     },
@@ -89,6 +95,15 @@ async function setCardsField(cardId) {
 
     await updateScore();
     await drawButton(duelResults);
+}
+
+async function removeAllCardsImages(){
+    let { computerBox, player1Box} = state.playerSides;
+    let imgElements = computerBox.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+
+    imgElements = player1Box.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
 }
 
 async function drawSelectedCard(index) {
